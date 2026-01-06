b5946447994054a8c7dd0ce04a74bd706b7bb9a1
lengoclinh-dev0608 <lnlinh0000@gmail.com>
2026-01-06 09:11:24 +0700

    fix: vercel deployment (css, routing, html)

--- index.tsx
+++ index.tsx
@@ -5,6 +5,7 @@ import { WagmiProvider } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import '@rainbow-me/rainbowkit/styles.css';
+import './index.css';
 import App from './App';
 import { config } from './lib/wagmi-config';
 
